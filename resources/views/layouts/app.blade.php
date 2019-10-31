a0cfe7c6a2b9b324f061ccb60ad9307305e8c52d
Lucas S <lucas.siqueira.araujo@gmail.com>
2019-10-31 16:34:41 -0300

    bug fix logos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,14 +56,14 @@
                 <a target="_blank" href="http://lmts.uag.ufrpe.br/"><img src="{{asset('images/lmts.jpg')}}" style = "margin-left: 8px; margin-top:30px " height="70%"  align = "right" ></a>
 
                 <img src="{{asset('images/separador.png')}}" style = "margin-left: 15px; margin-top: 30px" height="70%" align = "right" >
-                <a target="_blank" href="http://www.preg.ufrpe.br/"><img src="{{asset('images/logoPreg.png')}}" style = "margin-left: 10px; margin-top: 30px" height="70%" width="10%" align = "right" ></a>
+                <a target="_blank" href="http://www.preg.ufrpe.br/"><img src="{{asset('images/logoPreg.png')}}" style = "margin-left: 10px; margin-top: 30px" height="70%"  align = "right" ></a>
 
 
                 <img src="{{asset('images/separador.png')}}" style = "margin-left: 15px; margin-top: 30px" height="70%" align = "right" >
-                <a target="_blank" href="http://ww3.uag.ufrpe.br/"><img src="{{asset('images/uag.png')}}" style = "margin-left: 10px; margin-top: 30px" height="60%" width="5%" align = "right" ></a>
+                <a target="_blank" href="http://ww3.uag.ufrpe.br/"><img src="{{asset('images/uag.png')}}" style = "margin-left: 10px; margin-top: 30px" height="60%"  align = "right" ></a>
 
                 <img src="{{asset('images/separador.png')}}" style = "margin-left: 15px; margin-top: 30px" height="70%" align = "right" >
-                <a target="_blank" href="http://www.ufrpe.br/"><img src="{{asset('images/ufrpe.png')}}" style = "margin-left: 15px; margin-right: -10px; margin-top: 30px " height="70%" width="5%" align = "right"></a>
+                <a target="_blank" href="http://www.ufrpe.br/"><img src="{{asset('images/ufrpe.png')}}" style = "margin-left: 15px; margin-right: -10px; margin-top: 30px " height="70%"  align = "right"></a>
             </li>
         </ul>
       </div>
