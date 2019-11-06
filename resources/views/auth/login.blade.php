55ca0ccb0df1989d6acfe159724911027dd8b7fc
pc08 <pc08@lmts.com>
2019-11-06 18:53:35 -0300

    tela login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,205 +1,200 @@
-@extends('layouts.app')
+@extends('layouts.viewLogin')
 @section('titulo','Entrar')
 
 @section('content')
-<div class="info" >
-    <div class="info-texto" >
-        <div style="">
-          <div class="card">
-              <div class="titulo-tabela-lmts">
-                <h2>Editais Abertos</h2>
-              </div>
-              <div class="card-body">
-                <table class="table table-ordered table-hover">
-                  <?php $editaisAbertos = true;
-                        $editaisAbertosFlag = true;
-                        $editaisFinalizadosFlag = true; ?>
-                  @foreach ($editais as $edital)
-                    <?php if($edital->resultadoFinal <= $mytime){
-                      $editaisAbertos = false;
-                    }
-                    else{
-                      $editaisAbertos = true;
-                    }
-                    ?>
-                    @if($editaisAbertos)
-                      @if($editaisAbertosFlag)
-                        <tr style="background-color: #F7F7F7">
-                          <th> Nome</th><?php $editaisAbertosFlag = false;?>
-                          <th> Publicado em </th>
-                          <th> Arquivo </th>
-                        </tr>
-                      @endif
-                    @else
-                      @if($editaisFinalizadosFlag)
-                      </table>
-                      </div>
-                      <div class="titulo-tabela-lmts">
-                        <h2>Editais Finalizados</h2>
-                      </div>
-                      <div class="card-body">
-                      <table class="table table-ordered table-hover">
-                        <tr style="background-color: #F7F7F7">
-                          <th> Nome</th><?php $editaisFinalizadosFlag = false;?>
-                          <th> Publicado em </th>
-                          <th> Arquivo </th>
-                        </tr>
-                      @endif
-                    @endif
-                    <tr>
 
-                      <td style="width: 60rem">
-                        <div class="hover-popup-lmts">   <!-- time line  class="hover-popup-lmts"-->
-                         <a>
-                           <?php
-                             $nomeEdital = explode(".pdf", $edital->nome);
-                             echo ($nomeEdital[0]);
-                            ?>
-                           <span>
-                             <img src="<?php
-                              if($edital->inicioIsencao > $mytime){
-                                echo (asset('images/timeline1.png'));
-                              }
+<style media="screen">
+  #login{
+    height: 80vh;
+  }
+  #btn{
+    margin-top: -6%;
+    margin-left: 60%;
+  }
+  #link{
+    margin-left: 15%;
+  }
+  @media screen and (max-width: 576px){
+    #login{
+      height: 100vh;
+    }
+    #btn{
+      margin-top: -10%;
+      margin-left: 60%;
+    }
+    #link{
+      margin-top: 10%;
+      margin-left: 3%;
 
-                              elseif(($edital->inicioIsencao <= $mytime) && ($edital->fimIsencao >= $mytime)){
-                                  echo (asset('images/timeline2.png'));
-                              }
+    }
 
-                              elseif(($edital->inicioRecursoIsencao <= $mytime) && ($edital->fimRecursoIsencao >= $mytime)){
-                                  echo (asset('images/timeline3.png'));
+  }
+</style>
+<!-- box-shadow: inset 0 0 7px rgba(0,0,0,0.5); -->
 
-                              }
-
-                              elseif(($edital->inicioInscricoes <= $mytime) && ($edital->fimInscricoes >= $mytime)){
-                                  echo (asset('images/timeline4.png'));
-
-                              }
-                              elseif(($edital->inicioRecurso <= $mytime) && ($edital->fimRecurso >= $mytime)){
-                                  echo (asset('images/timeline5.png'));
-                              }
-                              elseif($edital->fimRecurso <= $mytime){
-                                echo (asset('images/timeline6.png'));
-                              }
+<div class="container-fluid" style=" box-shadow: inset 0 0 7px rgba(0,0,0,0.5);">
+  <div id="row1" class="row" style="">
 
 
-                             ?>" alt="image" height="140"/>
-                           </span>
-                         </a>
-
-                        </div>
-                      </td>
-                      <td> <!-- data -->
-                        <?php
-                          $date = date_create($edital->dataPublicacao);
-                         ?>
-                        <a>{{ date_format($date , 'd/m/y')  }}</a>
-                      </td>
-                      <td> <!-- Download -->
-                        <a href="{{ route('download', ['file' => $edital->pdfEdital])}}" target="_parent">Baixar Edital</a>
-                      </td>
-
-                    </tr>
-                  @endforeach
-
+    <!-- editais -->
+    <div id="editais" class="col-sm-9  container-fluid" style="">
+        <!-- titulo editais Abertos -->
+        <div class="titulo-tabela-lmts">
+          <h2>Editais Abertos</h2>
+        </div><!-- end titulo editais Abertos -->
+        <!-- table editais abertos  -->
+        <table class="table table-ordered table-hover">
+          <?php $editaisAbertos = true;
+                $editaisAbertosFlag = true;
+                $editaisFinalizadosFlag = true; ?>
+          @foreach ($editais as $edital)
+            <?php if($edital->resultadoFinal <= $mytime){
+              $editaisAbertos = false;
+            }
+            else{
+              $editaisAbertos = true;
+            }
+            ?>
+            @if($editaisAbertos)
+              @if($editaisAbertosFlag)
+                <tr style="background-color: #F7F7F7">
+                  <th> Nome</th><?php $editaisAbertosFlag = false;?>
+                  <th> Publicado em </th>
+                  <th> Arquivo </th>
+                </tr>
+              @endif
+            @else
+              @if($editaisFinalizadosFlag)
                 </table>
-              </div>
-              <div class="card-body">
-                {{ $editais->links() }}
-              </div>
+                <div class="titulo-tabela-lmts">
+                  <h2>Editais Finalizados</h2>
+                </div>
+                <table class="table table-ordered table-hover">
+                  <tr style="background-color: #F7F7F7">
+                    <th> Nome</th><?php $editaisFinalizadosFlag = false;?>
+                    <th> Publicado em </th>
+                    <th> Arquivo </th>
+                  </tr>
+              @endif
+            @endif
+            <tr>
+
+              <td style="width: 60rem">
+                 <a>
+                   <?php
+                     $nomeEdital = explode(".pdf", $edital->nome);
+                     echo ($nomeEdital[0]);
+                    ?>
+                 </a>
+              </td>
+              <td> <!-- data -->
+                <?php
+                  $date = date_create($edital->dataPublicacao);
+                 ?>
+                <a>{{ date_format($date , 'd/m/y')  }}</a>
+              </td>
+              <td> <!-- Download -->
+                <a href="{{ route('download', ['file' => $edital->pdfEdital])}}" target="_parent">Baixar Edital</a>
+              </td>
+
+            </tr>
+          @endforeach
+
+        </table>
+
+      <div class="card-body">
+        {{ $editais->links() }}
+      </div>
+
+    </div><!-- end editais -->
+
+    <!-- login -->
+    <div id="login" class="col-sm-3" style="background-color:#EEE;">
+      <form method="POST" action="{{ route('loginApi') }}">
+          @csrf
+
+        <!-- nome entrar -->
+        <div class="row justify-content-center" style="">
+          <h2 class="text-center">Entrar</h2>
+        </div><!-- end nome entrar -->
+
+        <!-- email -->
+        <div class="row justify-content-center" style="">
+          <div class="col-md-8">
+              <label for="email" class="field a-field a-field_a3 page__field" style="width:100%">
+              <input id="email" type="email" class="form-control @error('email') is-invalid @enderror field__input a-field__input"
+              name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-Mail">
+
+              <span class="a-field__label-wrap">
+                  <span class="a-field__label">E-mail</span>
+              </span>
+              </label>
+              @error('email')
+              <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
+              <strong>{{ $message }}</strong>
+              </span>
+              @enderror
           </div>
-        </div>
+        </div><!-- end email -->
+
+        <!-- senha -->
+        <div class="row justify-content-center">
+          <div class="col-md-8">
+              <label for="password" class="field a-field a-field_a3 page__field" style="width:100%" >
+              <input id="password" type="password" class="form-control @error('password') is-invalid @enderror field__input a-field__input"
+              name="password" required autocomplete="current-password" placeholder="Senha">
+
+              <span class="a-field__label-wrap">
+                  <span class="a-field__label">Senha</span>
+              </span>
+              </label>
+              @error('password')
+              <span class="invalid-feedback" role="alert" style="overflow: visible; display:block;">
+              <strong>{{ $message }}</strong>
+              </span>
+              @enderror
+          </div>
+        </div><!-- end senha -->
+
+        <!-- row lembre-se de mim | Esqueceu senha -->
+        <div class="row justify-content-center">
+          <div class="col-md-8">
+              <div class="form-check">
+                  <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                  <label class="form-check-label" for="remember">
+                      {{ __('Lembre-se de mim') }}
+                  </label>
+              </div>
+              @if (Route::has('password.request'))
+              <a class="btn btn-link" href="#" style="color: #1B2E4F; opacity: 0">
+                {{ __('Esqueceu sua senha?   ') }}
+              </a>
+              @endif
+          </div>
+        </div><!-- end row lembre-se de mim | Esqueceu senha -->
+
+        <!-- botões -->
+        <div class="container">
+
+          <div id="link" class="col-sm-5">
+            <a class="menu-principal" href="{{route('register')}}" style="color: #1B2E4F; ">Cadastrar</a>
+          </div>
+
+          <div id="btn" class="col-sm-3">
+              <button type="submit" class="btn btn-primary"  style="background-color: #1B2E4F; border-color: #d3e0e9">
+                  {{ __('Entrar') }}
+              </button>
+          </div>
+        </div><!-- end botões -->
 
 
-    </div>
+
+      </form>
+    </div><!-- end login -->
 
 
-    <div class="info-login" style="box-shadow: inset 0 0 7px rgba(0,0,0,0.5);">
-
-            <h2 class="text-center">Entrar</h2>
-
-            <form method="POST" action="{{ route('loginApi') }}">
-                @csrf
-                <div class="form-group">
-                    <!--
-                    <label for="exampleInputEmail1">E-mail</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
-                    -->
-
-                    <!-- Form E-mail -->
-                    <div class="form-group row formulario-centro">
-
-                        <div class="col-md-9">
-                            <label for="email" class="field a-field a-field_a3 page__field ">
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror field__input a-field__input"
-                            name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-Mail">
-
-                            <span class="a-field__label-wrap">
-                                <span class="a-field__label">E-mail</span>
-                            </span>
-                            </label>
-                            @error('email')
-                            <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
-                            <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                    </div>
-
-
-                </div>
-
-                <!-- Form Senha -->
-                <div class="form-group row formulario-centro">
-
-                    <div class="col-md-9">
-                        <label for="password" class="field a-field a-field_a3 page__field" >
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror field__input a-field__input"
-                        name="password" required autocomplete="current-password" placeholder="Senha">
-
-                        <span class="a-field__label-wrap">
-                            <span class="a-field__label">Senha</span>
-                        </span>
-                        </label>
-                        @error('password')
-                        <span class="invalid-feedback" role="alert" style="overflow: visible; display:block;">
-                        <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="form-group row formulario-centro" style="padding-left:70px">
-                        <div class="col-md-6 ">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                <label class="form-check-label" for="remember">
-                                    {{ __('Lembre-se de mim') }}
-                                </label>
-                            </div>
-                            @if (Route::has('password.request'))
-                            <a class="btn btn-link" href="#" style="color: #1B2E4F; opacity: 0">
-                              {{ __('Esqueceu sua senha?   ') }}
-                            </a>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="form-group row mb-0 justify-content-center ">
-                        <div class="row " style="margin-top:20px">
-                                <div class="col-md-6 " style="">
-                                <a class="menu-principal" href="{{route('register')}}" style="color: #1B2E4F; margin-left: 10px">Cadastrar</a>
-                                </div>
-
-                                <div class="col-md-6 " style="margin-left: -14px; margin-top: -4px">
-                                    <button type="submit" class="btn btn-primary"  style="margin-left: 100px;background-color: #1B2E4F; border-color: #d3e0e9">
-                                        {{ __('Entrar') }}
-                                    </button>
-                                </div>
-                        </div>
-
-                </div>
-
-            </form>
-    </div>
+  </div>
 </div>
+
 @endsection
