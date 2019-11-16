b04cc92ba641a1d68a3f73d2e230afc0248a8448
Lucas <lucas.siqueira.araujo@gmail.com>
2019-11-16 16:28:17 -0300

    feature 358

--- resources/views/cadastrarInscricao.blade.php
+++ resources/views/cadastrarInscricao.blade.php
@@ -54,7 +54,7 @@
 
 
 <div class="container">
-  <form class="" method="POST" action="{{ route('cadastroInscricao') }}" enctype="multipart/form-data">
+  <form autocomplete="off" method="POST" action="{{ route('cadastroInscricao') }}" enctype="multipart/form-data">
     @csrf
     <input type="hidden" name="editalId" value="{{$editalId}}" />
     <input id="tipo" type="hidden" name="tipo" value=""/>
@@ -199,7 +199,8 @@
                     <div class="col-sm-8" id="selectCurso">
 
                       <select class="form-control col-sm-10" name="curso" style="width: 100%">
-                        <?php foreach ($cursosDisponiveis as $curso) {
+                        <?php
+                        foreach ($cursosDisponiveis as $curso) {
                           if($curso[0] != '#'){
                             if($curso[0] != ''){
                               echo('<option value=' . $curso[2] . '>' . $curso[0] . '</option>');
@@ -237,7 +238,7 @@
                       </div>
                     </div>
                     <div class="row">
-                      <div class="col-sm-12">
+                      <div class="col-sm-12 autocomplete">
                           <input id="polo" type="text" name="polo" autofocus class="form-control @error('polo') is-invalid @enderror field__input a-field__input" placeholder="Polo (apenas aluno EAD):" style="width: 100%;" value="{{ old('polo') }}" style="width:100%">
 
                       </div>
@@ -415,130 +416,235 @@
 
 <script type="text/javascript" >
 
-function comprovanteSelecionado(){
-  document.getElementById("formulario").style.display = "";
-}
+  function comprovanteSelecionado(){
+    document.getElementById("formulario").style.display = "";
+  }
 
-function escolherTipo(x) {
-	if (x == "reintegracao") {
-   document.getElementById("tipo").value = "reintegracao";
-   document.getElementById("historicoEscolar").style.display = "";
-   document.getElementById("declaracaoDeVinculo").style.display = "none";
-   document.getElementById("enem").style.display = "none";
-   document.getElementById("curriculo").style.display = "none";
-   document.getElementById("programaDasDisciplinas").style.display = "none";
+  function escolherTipo(x) {
+  	if (x == "reintegracao") {
+     document.getElementById("tipo").value = "reintegracao";
+     document.getElementById("historicoEscolar").style.display = "";
+     document.getElementById("declaracaoDeVinculo").style.display = "none";
+     document.getElementById("enem").style.display = "none";
+     document.getElementById("curriculo").style.display = "none";
+     document.getElementById("programaDasDisciplinas").style.display = "none";
 
-   if(document.getElementById("comprovante").value == 'isento'){
-     document.getElementById("formulario").style.display = "";
-   }
+     if(document.getElementById("comprovante").value == 'isento'){
+       document.getElementById("formulario").style.display = "";
+     }
 
-	}
-	if (x == "transferenciaInterna") {
-    document.getElementById("tipo").value = "transferenciaInterna";
-    document.getElementById("historicoEscolar").style.display = "";
-    document.getElementById("declaracaoDeVinculo").style.display = "";
-    document.getElementById("enem").style.display = "none";
-    document.getElementById("curriculo").style.display = "none";
-    document.getElementById("programaDasDisciplinas").style.display = "none";
+  	}
+  	if (x == "transferenciaInterna") {
+      document.getElementById("tipo").value = "transferenciaInterna";
+      document.getElementById("historicoEscolar").style.display = "";
+      document.getElementById("declaracaoDeVinculo").style.display = "";
+      document.getElementById("enem").style.display = "none";
+      document.getElementById("curriculo").style.display = "none";
+      document.getElementById("programaDasDisciplinas").style.display = "none";
 
-    if(document.getElementById("comprovante").value == 'isento'){
-      document.getElementById("formulario").style.display = "";
-    }
-	}
-	if (x == "transferenciaExterna") {
-    document.getElementById("tipo").value = "transferenciaExterna";
-    document.getElementById("historicoEscolar").style.display = "";
-    document.getElementById("declaracaoDeVinculo").style.display = "";
-    document.getElementById("enem").style.display = "none";
-    document.getElementById("curriculo").style.display = "";
-    document.getElementById("programaDasDisciplinas").style.display = "";
+      if(document.getElementById("comprovante").value == 'isento'){
+        document.getElementById("formulario").style.display = "";
+      }
+  	}
+  	if (x == "transferenciaExterna") {
+      document.getElementById("tipo").value = "transferenciaExterna";
+      document.getElementById("historicoEscolar").style.display = "";
+      document.getElementById("declaracaoDeVinculo").style.display = "";
+      document.getElementById("enem").style.display = "none";
+      document.getElementById("curriculo").style.display = "";
+      document.getElementById("programaDasDisciplinas").style.display = "";
 
-    if(document.getElementById("comprovante").value == 'isento'){
-      document.getElementById("formulario").style.display = "";
-    }
-	}
-	if (x == "portadorDeDiploma") {
-    document.getElementById("tipo").value = "portadorDeDiploma";
-    document.getElementById("historicoEscolar").style.display = "";
-    document.getElementById("declaracaoDeVinculo").style.display = "";
-    document.getElementById("enem").style.display = "";
-    document.getElementById("curriculo").style.display = "none";
-    document.getElementById("programaDasDisciplinas").style.display = "";
+      if(document.getElementById("comprovante").value == 'isento'){
+        document.getElementById("formulario").style.display = "";
+      }
+  	}
+  	if (x == "portadorDeDiploma") {
+      document.getElementById("tipo").value = "portadorDeDiploma";
+      document.getElementById("historicoEscolar").style.display = "";
+      document.getElementById("declaracaoDeVinculo").style.display = "";
+      document.getElementById("enem").style.display = "";
+      document.getElementById("curriculo").style.display = "none";
+      document.getElementById("programaDasDisciplinas").style.display = "";
 
-    if(document.getElementById("comprovante").value == 'isento'){
-      document.getElementById("formulario").style.display = "";
-    }
-	}
-}
+      if(document.getElementById("comprovante").value == 'isento'){
+        document.getElementById("formulario").style.display = "";
+      }
+  	}
+  }
 
 
+  //cep
+  function limpa_formulário_cep() {
+          //Limpa valores do formulário de cep.
+          document.getElementById('rua').value=("");
+          document.getElementById('bairro').value=("");
+          document.getElementById('cidade').value=("");
+          document.getElementById('uf').value=("");
+  }
 
-    function limpa_formulário_cep() {
-            //Limpa valores do formulário de cep.
-            document.getElementById('rua').value=("");
-            document.getElementById('bairro').value=("");
-            document.getElementById('cidade').value=("");
-            document.getElementById('uf').value=("");
-    }
+  function meu_callback(conteudo) {
+      if (!("erro" in conteudo)) {
+          //Atualiza os campos com os valores.
+          document.getElementById('rua').value=(conteudo.logradouro);
+          document.getElementById('bairro').value=(conteudo.bairro);
+          document.getElementById('cidade').value=(conteudo.localidade);
+          document.getElementById('uf').value=(conteudo.uf);
 
-    function meu_callback(conteudo) {
-        if (!("erro" in conteudo)) {
-            //Atualiza os campos com os valores.
-            document.getElementById('rua').value=(conteudo.logradouro);
-            document.getElementById('bairro').value=(conteudo.bairro);
-            document.getElementById('cidade').value=(conteudo.localidade);
-            document.getElementById('uf').value=(conteudo.uf);
+      } //end if.
+      else {
+          //CEP não Encontrado.
+          limpa_formulário_cep();
+          alert("CEP não encontrado.");
+      }
+  }
 
-        } //end if.
-        else {
-            //CEP não Encontrado.
-            limpa_formulário_cep();
-            alert("CEP não encontrado.");
+  function pesquisacep(valor) {
+
+      //Nova variável "cep" somente com dígitos.
+      var cep = valor.replace(/\D/g, '');
+
+      //Verifica se campo cep possui valor informado.
+      if (cep != "") {
+
+          //Expressão regular para validar o CEP.
+          var validacep = /^[0-9]{8}$/;
+
+          //Valida o formato do CEP.
+          if(validacep.test(cep)) {
+
+              //Preenche os campos com "..." enquanto consulta webservice.
+              document.getElementById('rua').value="...";
+              document.getElementById('bairro').value="...";
+              document.getElementById('cidade').value="...";
+              document.getElementById('uf').value="...";
+
+
+              //Cria um elemento javascript.
+              var script = document.createElement('script');
+
+              //Sincroniza com o callback.
+              script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+              //Insere script no documento e carrega o conteúdo.
+              document.body.appendChild(script);
+
+          } //end if.
+          else {
+              //cep é inválido.
+              limpa_formulário_cep();
+              alert("Formato de CEP inválido.");
+          }
+      } //end if.
+      else {
+          //cep sem valor, limpa formulário.
+          limpa_formulário_cep();
+      }
+  };
+
+  var polos = ["Limoeiro","Carpina","Vitória da Conquista","Afrânio","Surubim","Gravatá","Ilhéus","Palmares","Pesqueira","Camaçari","Tabira","Trindade",]
+
+
+  //autoCompĺete
+  function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
         }
-    }
-
-    function pesquisacep(valor) {
-
-        //Nova variável "cep" somente com dígitos.
-        var cep = valor.replace(/\D/g, '');
-
-        //Verifica se campo cep possui valor informado.
-        if (cep != "") {
-
-            //Expressão regular para validar o CEP.
-            var validacep = /^[0-9]{8}$/;
-
-            //Valida o formato do CEP.
-            if(validacep.test(cep)) {
-
-                //Preenche os campos com "..." enquanto consulta webservice.
-                document.getElementById('rua').value="...";
-                document.getElementById('bairro').value="...";
-                document.getElementById('cidade').value="...";
-                document.getElementById('uf').value="...";
-
-
-                //Cria um elemento javascript.
-                var script = document.createElement('script');
-
-                //Sincroniza com o callback.
-                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
-
-                //Insere script no documento e carrega o conteúdo.
-                document.body.appendChild(script);
-
-            } //end if.
-            else {
-                //cep é inválido.
-                limpa_formulário_cep();
-                alert("Formato de CEP inválido.");
-            }
-        } //end if.
-        else {
-            //cep sem valor, limpa formulário.
-            limpa_formulário_cep();
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
+          currentFocus++;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
+          currentFocus--;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          e.preventDefault();
+          if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+          }
         }
-    };
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+        }
+      }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+  }
+
+  //end autocomplete
+
+  autocomplete(document.getElementById("polo"), polos);
 
 </script>
 
