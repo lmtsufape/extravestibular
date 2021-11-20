36fc3ed6cbf10b5455d1ae41be2f05995ecf7f30
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2021-11-20 17:37:32 -0300

    atualiza arquivos por tipo de inscricao

---
+++ database/migrations/2021_11_20_174745_add_dados_to_inscricaos.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDadosToInscricaos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('inscricaos', function (Blueprint $table) {
+            $table->string('declaracaoDeVeracidade')->nullable();
+            $table->string('declaracaoENADE')->nullable();
+            $table->string('rg')->nullable();
+            $table->string('cpf')->nullable();
+            $table->string('quitacaoEleitoral')->nullable();
+            $table->string('reservista')->nullable();
+            $table->string('certidaoNascimento')->nullable();
+            $table->string('historicoEnsinoMedio')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('inscricaos', function (Blueprint $table) {
+            $table->dropColumn([
+                'declaracaoDeVeracidade',
+                'declaracaoENADE',
+                'rg',
+                'cpf',
+                'quitacaoEleitoral',
+                'reservista',
+                'certidaoNascimento',
+                'historicoEnsinoMedio']);
+        });
+    }
+}
